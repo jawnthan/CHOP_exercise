ccabf7dcd0baa57a9962f5c08ebdf186b28a78ac
John Whittaker <jawnthan@Johns-MacBook-Air.local>
2021-10-22 19:09:21 -0400

    added date constraint

--- my_code/parser.py
+++ my_code/parser.py
@@ -3,7 +3,9 @@ import os
 
 filename = os.path.expanduser('~/Desktop/CHOP_exercise/analyst-take-home-task-master/datasets/encounters.csv')
 
-date = 
+#encounter_date = 
+
+counter = 0
 
 with open(filename, 'r') as encounters_csv:
 
@@ -11,4 +13,14 @@ with open(filename, 'r') as encounters_csv:
 
    for row in encounters:
 
-      print(row)
+      if row["START"] >= "1999-07-15 00:00:00":
+
+         print(row["START"])
+
+         counter += 1
+
+   print(counter)
+#
+#         if counter == 9:
+#
+#            break
